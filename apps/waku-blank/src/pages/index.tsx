94becc8c72dc2a931e59fe20b7d71f0541b18eee
S-P-H-E <sphe.g.personal@gmail.com>
2025-11-10 17:13:40 +0200

    Update version to 1.0.34, remove package-lock.json files from expo-blank, node-blank, and nuxt-blank, and initialize git repository with an initial commit in Step5.

--- apps/waku-blank/src/pages/index.tsx
+++ apps/waku-blank/src/pages/index.tsx
@@ -1,4 +1,6 @@
 export default async function Home() {
+  // Use Jotai for global state management
+  
   return (
     <div className="h-dvh flex flex-col justify-center items-center">
       <h1 className="text-2xl font-semibold">Empty Waku Template By Sphe.</h1>
